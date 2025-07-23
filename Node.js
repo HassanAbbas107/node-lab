e3a6c75b6013739e476267d5e8179cd4637bba03
HassanAbbas107 <qiqbx223@gmail.com>
2025-07-23 09:09:40 +0300

    ok

--- Node.js
+++ Node.js
@@ -1,15 +1,18 @@
 const express =require("express");
 const { request } = require("node:http");
 const app = express();
-app.get("/.",(requset,response)=>{
-    response.sendFile(__dirname+"views/Home.html")
+
+
+
+app.get("/",(requset,response)=>{
+    response.sendFile(__dirname+"/views/Home.html")
 })
 
 app.get("/about",(requset,response)=>{
-    response.sendFile(__dirname+"views/index.html")
+    response.sendFile(__dirname+"/views/index.html")
 })
 app.get("/test",(requset,response)=>{
-    response.sendFile(__dirname+"views/test.html")
+    response.sendFile(__dirname+"/views/test.html")
 })
 
 
